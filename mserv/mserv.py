1fa3ed0fddf76b92d01df9281763c66f68d9f570
quinton <dropatuningmetal@gmail.com>
2020-12-17 20:50:06 -0700

    Progressbar implemented
    implemented 'click' as argument parser
    
    So far, back in working order. Prepare to merge with master

--- mserv/mserv.py
+++ mserv/mserv.py
@@ -3,35 +3,26 @@
 import os
 import requests
 import socket
-from bs4 import BeautifulSoup
-import argh
+import Libs.Networking as Networking
+import click
 import subprocess
-from clint.textui import progress
-from colorama import Fore, Back, Style, init
+from colorama import Fore, Style, init
+import toml
 
 init(autoreset=True)
 serverDir = {}
 url = "https://www.minecraft.net/en-us/download/server/"
+downloader = Networking.Networking(url)
+version_num = toml.load(os.path.join(os.pardir, 'pyproject.toml'))['tool']['poetry']['version']
 
 
-def file_webscraper(url=url, search_file='server.jar'):
-    """Searches a specified webpage searching for a hyperlink to a specified file
-    
-    Keyword Arguments:
-        url {str} -- Url of the webpage to scrape (default: {https://www.minecraft.net/en-us/download/server/})
-        search_file {str} -- The file to search for (default: {'server.jar'})
-    
-    Returns:
-        [str] -- The url of the file we want to access
-    """
-    requester = requests.get(url)
-    soupy = BeautifulSoup(requester.text, features="html.parser")
-    for link in soupy.findAll('a'):
-        if link.get("href") is not None:
-            if search_file in link.get("href"):
-                return link.get('href')
+@click.group()
+@click.version_option(version=version_num)
+def mserv_cli():
+    pass
 
-def identify_servers(path=os.getcwd()):
+
+def identify_servers():
     # Identify any potential servers in current directory
     for subdir, _, filenames in walklevel(os.getcwd()):
         if "server.jar" in filenames:
@@ -39,6 +30,7 @@ def identify_servers(path=os.getcwd()):
                 serverDir[os.path.basename(os.path.normpath(subdir))].append(subdir)
             else:
                 serverDir[os.path.basename(os.path.normpath(subdir))] = subdir
+
 
 # os.walk, but allows for level distinction
 def walklevel(some_dir, level=1):
@@ -52,53 +44,22 @@ def walklevel(some_dir, level=1):
             del dirs[:]
 
 
-def fileNameFromURL(url):
-    # Extracts the filename from a given url
-    if url.find('/'):
-        return url.rsplit('/', 1)[1]
+@click.command()
+def update(server_name):
+    """Download a fresh server.jar file from Mojang.
 
+    Scrapes the Mojang server download website for a new server.jar file.
+    This works whether or not the executable is new """
 
-def update(serverName):
-    """Goes to the official Mojang website and downloads the server.jar file again. This works whether or not the
-    executable is new """
     # identify where the server.jar file is located
-    os.remove(f"{os.path.join(serverDir[serverName], 'server.jar')}")
-    download_to_dir(file_webscraper(), serverDir[serverName])
+    os.remove(f"{os.path.join(serverDir[server_name], 'server.jar')}")
+    downloader.download_to_dir(downloader.file_webscraper(), serverDir[server_name])
 
 
-def download_to_dir(url, outDir=os.getcwd()):
-    """Downloads a file from a url and saves it in the specified output directory
-    
-    Arguments:
-        url {str} -- The url of the file to be downloaded
-    
-    Keyword Arguments:
-        outDir {str} -- Directory to save the file to (default: {os.getcwd()})
-    """
-    requestor = requests.get(url, stream=True)
-    fileName = fileNameFromURL(url)
-    directory = os.path.join(outDir, fileName)
-    # Exception handling for the HTTPS request
-    try:
-        requestor.raise_for_status()
-    except Exception as urlOof:
-        print(Fore.RED + "Error in accessing URL: %s", urlOof)
-        input("Press ENTER to continue...")
-
-    print(Fore.YELLOW + Style.BRIGHT + "Downloading %s" % fileName)
-    # Some exception handling for file writing stuff
-    with open(directory, "wb") as file:
-        total_length = int(requestor.headers.get('content-length'))
-        for chunk in progress.bar(requestor.iter_content(chunk_size=1024), expected_size=(total_length / 1024) + 1):
-            if chunk:
-                file.write(chunk)
-                file.flush()
-
-
-def eula_true(serverName):
+def eula_true(server_name):
     """Points to the eula.txt generated from the server executable, generates text to auto-accept the eula
     """
-    eula_dir = os.path.join(serverDir[serverName], 'eula.txt')
+    eula_dir = os.path.join(serverDir[server_name], 'eula.txt')
     # with is like your try .. finally block in this case
     with open(eula_dir, 'r') as file:
         # read a list of lines into data
@@ -117,78 +78,82 @@ def eula_true(serverName):
             file.writelines(data)
 
 
+@click.command()
 def setup():
-    """Runs functions that generate the server files before running
     """
-    serverName = input(Fore.YELLOW + Style.BRIGHT + "Input new server name: ")
-    os.mkdir(os.path.join(os.getcwd(), serverName))
-    download_to_dir(file_webscraper(), os.path.join(os.getcwd(), serverName))
+    Create a new server.
+    Runs functions that generate the server files before running.
+    """
+    new_server_name = input(Fore.YELLOW + Style.BRIGHT + "Input new server name: ")
+    new_server_dir = os.path.join(os.getcwd(), new_server_name)
+    os.mkdir(new_server_dir)
+    downloader.download_to_dir(new_dir=new_server_dir, scrape=True)
     identify_servers()
-    run(first_launch=True, serverName=serverName)
-    eula_true(serverName)
+    print(Fore.CYAN + Style.BRIGHT + '\nRunning first-time server setup...\n')
+    __run(first_launch=True, server_name=new_server_name)
+    eula_true(new_server_name)
     print(Fore.GREEN + Style.BRIGHT + '\nEULA Accepted and server is ready to go!!')
 
 
-def run(max_ram: "Maximum amount of ram alloted" = "-Xmx1024M", min_ram: "Minimum amount of ram alloted" = "-Xms1024M",
-        gui: "if True, will show the Mojang UI, else will remain CLI-based" = False,
-        first_launch: "Backend, used to denote if this is part of the Setup" = False, serverName='Server'):
+# Original run function, can operate independent of the argument parser
+def __run(max_ram="-Xmx1024M", min_ram="-Xms1024M", gui=False, server_name="Server", first_launch=False):
     """Executes the server binary with optional parameters
     """
-    # If multiple folders exist, let user select
-    #TODO
 
-    gui = "nogui" if gui is False else ""
+    ui = "nogui" if gui is False else ""
+
     if first_launch:
         subprocess.run(
-            ["java", f"{max_ram}", f"{min_ram}", "-jar", f"{os.path.join((serverDir[serverName]), 'server.jar')}", f"{gui}"],
-            cwd=serverDir[serverName], stdout=subprocess.DEVNULL)
+            ["java", f"{max_ram}", f"{min_ram}", "-jar", f"{os.path.join((serverDir[server_name]), 'server.jar')}",
+             f"{ui}"],
+            cwd=serverDir[server_name], stdout=subprocess.DEVNULL)
 
         return
 
     # List all identified server folders and let user select them
     identify_servers()
-    selectDir = ''
     if len(serverDir) > 1:
         print(f"{Fore.YELLOW}{Style.BRIGHT}Choose server to run (enter number): ")
         for number, item in enumerate(serverDir):
-            print(f"{number} - {item}  ", end=' ',)
+            print(f"{number} - {item}  ", end=' ', )
         selectDir = list(serverDir)[int(input())]
-        #TODO
     else:
-        selectDir=list(serverDir)[0]
+        selectDir = list(serverDir)[0]
     # Networking IP information
     print(Fore.GREEN + Style.BRIGHT + f"\nStarting {selectDir}\n")
     print(Fore.YELLOW + Style.BRIGHT + 'Gathering Network Information...\n')
     hostname = socket.gethostname()
     IP_Ad = requests.get('http://ip.42.pl/raw').text
-    print(
-        Fore.CYAN + Style.BRIGHT + f"Hostname: {hostname}\nIP Address: {IP_Ad}\nPort:25565")
-
+    print(Fore.CYAN + Style.BRIGHT + f"Hostname: {hostname}\nPublic IP Address: {IP_Ad}\nPort:25565")
     print("Starting Server...")
-    subprocess.run(["java", f"{max_ram}", f"{min_ram}", "-jar", f"{os.path.join(serverDir[selectDir], 'server.jar')}", f"{gui}"],
-                   cwd=serverDir[selectDir])
+    subprocess.run(
+        ["java", f"{max_ram}", f"{min_ram}", "-jar", f"{os.path.join(serverDir[selectDir], 'server.jar')}", f"{ui}"],
+        cwd=serverDir[selectDir])
 
 
-# TODO
+# Alias of the __run function that will be handled by the 'click' argument parser
+@click.command()
+@click.option('--max_ram', default="-Xmx1024M", help="Maximum amount of ram alloted")
+@click.option('--min_ram', help="Minimum amount of ram alloted", default="-Xms1024M")
+@click.option("--gui", default=False, help="'True', will show Mojang's UI, 'False', will remain CLI-based")
+@click.option("--server_name", help="force execution of other server directory", default='Server')
+def run(max_ram: str, min_ram: str, gui: bool, server_name: str, first_launch=False):
+    __run(max_ram, min_ram, gui, server_name, first_launch)
+
+
+# TODO implement GUI using pyqt
+@mserv_cli.command()
 def GUI():
-    """Executes the user interface for mserv, best for people who don't know how to use the command line
+    """Executes the user interface for mserv
     """
     pass
 
-def version():
-    """Displays the current version of the program
-    """
-    with open('../setup.py', 'r') as setup_file:
-        data = setup_file.readlines()
-    for line in data:
-        if 'version' in line:
-            print(Fore.MAGENTA + Style.BRIGHT+ f'mserv v{line[13:-3]}')
 
-
-def main():
-    parser = argh.ArghParser()
-    parser.add_commands([setup, run, update, version])
-    parser.dispatch()
+# Adding commands to the mserv_cli function for argument parsing
+mserv_cli.add_command(setup)
+mserv_cli.add_command(update)
+mserv_cli.add_command(run)
+mserv_cli.add_command(GUI)
 
 if __name__ == "__main__":
-    main()
+    mserv_cli()
